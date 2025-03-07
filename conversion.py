5d9f3c82ba851dfbf253c2cef7c5b8a1d25488a2
LouisaHob <louisahobson@yahoo.com>
2025-03-07 15:50:06 +0000

    Create conversion.py
    Compulsory Task 3

---
+++ conversion.py
@@ -0,0 +1,35 @@
+"""
+Store 99.23 into the variable num1
+Store 23 into the variable num2
+Store 150 into the variable num3
+Store "100" in the variable string1
+
+Convert the variable num1 into an int
+Convert the variable num2 into a float 
+Convert the variable num3 into a str
+Convert the variable string1 into an int
+
+Print all variables on seperate lines 
+"""
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+num1_int = int(num1)
+num2_float = float(num2)
+num3_str = str(num3)
+string1_int = int(string1)
+
+print(num1)
+print(num1_int)
+
+print(num2)
+print(num2_float)
+
+print(num3)
+print(num3_str)
+
+print(string1)
+print(string1_int)
