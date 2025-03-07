0bd185f56cf93e2940792c40d25b4b8f489da5ca
LouisaHob <louisahobson@yahoo.com>
2025-03-07 15:26:20 +0000

    Create hello_world.py
    Compulsory Task 1

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+""" Request user's name using input() function 
+Print users name 
+Request user's age using input() function
+Print users age 
+
+Print "Hello World!"
+"""
+
+name = input("Enter your name: ") 
+print(name)
+age = input("Enter your name: ")
+print(age)
+
+print("Hello World!")
